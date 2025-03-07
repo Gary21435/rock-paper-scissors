889cb5a9860d31aba287dabf1785b9d1c9a15e62
gegi <35646675+Gary21435@users.noreply.github.com>
2025-03-06 18:28:27 -0800

    Add a couple small features; attempt an injection attack and prevention

--- script.js
+++ script.js
@@ -20,30 +20,62 @@ let score2 = 0;
 
 let play;
 
+function displayScore(){
+    console.log(`Score. You: ${score1}, Computer: ${score2}`);
+}
 
-btn.addEventListener("click", function() {
+const btn = document.querySelector("#btn");
+
+function malFunction(e){
+    play = document.querySelector("#input").value;
+    const p = document.createElement("p");
+    const adiv = document.querySelector("div");
+    adiv.appendChild(p);
+    //if (e.isTrusted)
+    adiv.innerHTML = play;
+    p.textContent = play;
+
+    
+}
+
+function handleShit(){
     if(score1 >= 3 || score2 >= 3)
         return;
+
     play = document.querySelector("#input").value; //I had forgotten to put .value, which was why it 
     //wasn't working for a bit
     console.log(`You played: ${play}`);
+
+    
+
+
+
     if (play !== "rock" && play !== "paper" && play !== "scissors") {
-        console.error("wrong entry");
+        console.log("wrong entry");
         return;
     }
+    
 
     let comp = getComputerChoice();
-    console.log(comp);
+    console.log("Computer played: "+ comp);
 
-    if (play == comp) return;
+    if (play == comp) {
+        displayScore();
+        return;}
     if ((play == "rock" && comp == "scissors") || (play == "paper" && comp == "rock") || 
     (play == "scissors" && comp == "paper")) score1++;
     else score2++;
-    console.log(`Score. You: ${score1}, Computer: ${score2}`);
+    displayScore();
     
     if(score1 == 3 || score2 == 3){
         if(score1 > score2) console.log("YOU WIN");
         else console.log("YOU LOSE :/");
     }
+}
+
+btn.addEventListener("click", (e) => malFunction(e)); // you just put the name of the function with no ()
+                                           // in JS, a function() will execute wherever it is placed.
+document.addEventListener("keypress", function(e){
+    if(e.key === "Enter") handleShit();
 });
 
